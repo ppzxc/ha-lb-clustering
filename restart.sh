8439bfed0715991743aba623ea1ed2247b84477c
ppzxc <cjh8487@naver.com>
2021-03-18 22:26:29 +0900

    2021-03-18 fix timezone

--- restart.sh
+++ restart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export $(cat ./default.env)
 
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
 if [ $(docker network ls | grep halb-cluster | wc -l) -eq 0 ];then
 	docker network create halb-cluster
 fi
